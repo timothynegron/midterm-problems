bc75264395332bd985a77c930182d97c8d9c2799
Timothy Negron <timothy.negron@codeimmersives.com>
2021-03-08 16:09:26 -0500

    Adds functions.

--- main.js
+++ main.js
@@ -1,45 +1,125 @@
-function isEvenlyDivisible() {
+function isEvenlyDivisible(num1, num2) {
+  return num1 % num2 === 0 ? true : false;
 }
 
-function halfSquare() {
+function halfSquare(num) {
+  return (num * num) / 2;
 }
 
-function isLong() {
+function exclaim(str) {
+  return str.replaceAll("!", "") + "!";
 }
 
-function exclaim() {
+function isLong(str) {
+  if (str.length >= 15) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function countWords() {
+function countWords(words) {
+  let count = 1;
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === " ") {
+      count++;
+    }
+  }
+  return count;
 }
 
-function containsDigit() {
+function containsDigit(str) {
+  if (str === "") {
+    return false;
+  }
+  const numbers = "1234567890";
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (str[i] === numbers[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
-function containsLowerCase() {
+function containsSpace(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === " ") {
+      return true;
+    }
+  }
+  return false;
 }
 
-function containsUpperCase() {
+function digits(num) {
+  if (num < 0) {
+    num *= -1;
+  }
+  let arr = num.toString().split(".").join("");
+  return Array.from(arr).map((i) => Number(i));
+}
+function truncate(str) {
+  return str.length < 15 ? str : str.slice(0, 8) + "...";
+}
+function containsLowerCase(str) {
+  if (str.length === 0) {
+    return false;
+  } else if (/[a-z]/.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function containsUpperCase(str) {
+  if (str.length === 0) {
+    return false;
+  } else if (/[A-Z]/.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function containsNonAlphanumeric() {
+function containsNonAlphanumeric(str) {
+  if (str.length === 0) {
+    return false;
+  } else if (/[^a-zA-Z0-9]/.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function containsSpace() {
+function isValidPassword(password) {
+  if (!containsLowerCase(password)) {
+    return false;
+  }
+  if (!containsUpperCase(password)) {
+    return false;
+  }
+  if (/\s/.test(password)) {
+    return false;
+  }
+  if (!/[0-9]/.test(password)) {
+    return false;
+  }
+  if (!containsNonAlphanumeric(password)) {
+    return false;
+  }
+  return true;
 }
 
-function digits() {
+function onlyPunchy(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let temp = arr[i].replaceAll("!", "");
+    if (temp.length < 15) {
+      newArr.push(exclaim(arr[i]));
+    }
+  }
+  return newArr;
 }
-
-function truncate() {
-}
-
-function isValidPassword() {
-}
-
-function onlyPunchy() {
-}
-
 
 module.exports = {
   isEvenlyDivisible,
@@ -56,4 +136,4 @@ module.exports = {
   truncate,
   isValidPassword,
   onlyPunchy,
-}
+};
